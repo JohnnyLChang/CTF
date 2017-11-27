491b13bca1702143604395d3b6219d5aa44a2f2e
LFChang <riversnow@gmail.com>
2017-11-28 01:25:24 +0800

    remove unused files

---
+++ large.sh
@@ -0,0 +1,8 @@
+git rev-list master | while read rev; do git ls-tree -lr $rev | cut -c54- | grep -v '^ '; done | sort -u | perl -e '
+  while (<>) {
+    chomp;
+    @stuff=split("\t");
+    $sums{$stuff[1]} += $stuff[0];
+  }
+  print "$sums{$_} $_\n" for (keys %sums);
+' | sort -rn >> large_files.txt
